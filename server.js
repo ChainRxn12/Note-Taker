a2e88269f69c28e0ed730c7cb64abfb0349aaf22
Billy Dorman <b.d.dorman12@gmail.com>
2021-07-10 09:18:28 -0500

    got port listener working

--- server.js
+++ server.js
@@ -6,3 +6,33 @@ const express = require('express');
 const app = express();
 
 const allNotes = require('./db/db.json');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+
+// port listener
+app.listen(PORT, () => console.log(`Server Listening on port ${PORT}`));
